a213862b8e81cd858e365b2605fe344b34f2df1c
Nicholas Hickman <nicholas.hickman@edu.keuda.fi>
2024-08-08 08:54:49 +0300

    6

--- Harjoituksia A osa 6/Harjoituksia A osa 6/Program.cs
+++ Harjoituksia A osa 6/Harjoituksia A osa 6/Program.cs
@@ -1,4 +1,7 @@
-﻿//Pyydetään käyttäjän nimi jotta voimme tervehtia häntä
-Console.WriteLine(Kirjoita nimesi tähän.);
-Console.ReadLine();
-Console.WriteLine(Hei ""!);
+﻿string nimi;
+
+//Pyydetään käyttäjän nimi jotta voimme tervehtia häntä
+Console.Write("Kirjoita nimesi tähän: ");
+nimi = Console.ReadLine();
+Console.WriteLine("");
+Console.WriteLine("Hei " + nimi + "!");
